562251a6280161df9cb2ffaaefbd4ff640be81db
aceopmgit <ace.opm.sales@gmail.com>
2023-11-07 11:07:10 +0530

    How the middleware works

---
+++ app.js
@@ -0,0 +1,21 @@
+const https=require('http');
+
+const express=require('express')
+
+const app=express();
+
+app.use((req,res,next)=>{
+    console.log("In the middleware");
+    next();
+})
+
+app.use((req,res,next)=>{
+    console.log("In another middleware");
+    res.send('<h1>Hello from express js</h1>')    
+})
+
+
+const server=https.createServer(app)
+
+server.listen(4000);
+
